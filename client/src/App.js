2f79e54b3544a568a35bb2e82d45b669034681bd
Gurwinder Singh <guri60343@gmail.com>
2021-06-22 19:33:51 +0530

    setup routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import React from 'react';
+import Home from './components/Home';
 import './css/App.css';
+import Download from './components/Download';
+import {Switch, Route} from "react-router-dom";
 
 function App() {
-  return (
-    null
+  return (<>
+        <Switch>
+            <Route exact path='/'>
+                <Home/>
+            </Route>
+
+            <Route exact path='/download/:id'>
+                <Download/>
+            </Route>
+        </Switch>
+    </>
   );
 }
 
